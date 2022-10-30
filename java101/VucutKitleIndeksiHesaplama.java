4efa737747bccaa48072e197b1c4ce06de859cbb
aysegul-sofuoglu <73445450+aysegul-sofuoglu@users.noreply.github.com>
2022-10-30 23:57:04 +0300

    body mass index calculator

---
+++ java101/VucutKitleIndeksiHesaplama.java
@@ -0,0 +1,23 @@
+package java101;
+import java.util.Scanner;
+
+public class VucutKitleIndeksiHesaplama {
+
+	public static void main(String[] args) {
+		
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+		double boy = input.nextDouble();
+		
+		System.out.println("Lütfen kilonuzu giriniz: ");
+		double kilo = input.nextDouble();
+		
+		double indeks = kilo / (boy * boy);
+		
+		System.out.println("Vücut kitle indeksiniz: " + indeks);
+		
+	}
+
+}
