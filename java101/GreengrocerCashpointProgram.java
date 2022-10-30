0a7c250810075a3107d3a3cf4d2a624234b86412
aysegul-sofuoglu <73445450+aysegul-sofuoglu@users.noreply.github.com>
2022-10-31 00:48:37 +0300

    greengrocer cash point program

---
+++ java101/GreengrocerCashpointProgram.java
@@ -0,0 +1,60 @@
+package java101;
+import java.util.Scanner;
+
+
+public class GreengrocerCashpointProgram {
+
+	public static void main(String[] args) {
+	
+		//total prices of fruits
+		
+		double pearPrice;
+		double applePrice;
+		double tomatoesPrice;
+		double bananaPrice;
+		double auberginePrice;
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		//calculation of the total prices of fruits
+		
+		System.out.println("Armut kaç kilo? ");
+		double kgPear = input.nextDouble();
+		
+		pearPrice = 2.14 * kgPear;
+		
+		
+		System.out.println("Elma kaç kilo? ");
+		double kgApple = input.nextDouble();
+		
+		applePrice = 3.67 * kgApple;
+		
+		
+		System.out.println("Domates kaç kilo? ");
+		double kgTomatoes = input.nextDouble();
+		
+		tomatoesPrice = 1.11 * kgTomatoes;
+		
+		
+		System.out.println("Muz kaç kilo? ");
+		double kgBanana = input.nextDouble();
+		
+		bananaPrice = 0.95 * kgBanana;
+		
+		
+		System.out.println("Patlıcan kaç kilo? ");
+		double kgAubergine = input.nextDouble();
+		
+		auberginePrice = 5.00 * kgAubergine;
+		
+		
+		//total shopping price
+		
+		double total = pearPrice + applePrice + tomatoesPrice + bananaPrice + auberginePrice;
+		System.out.println("Toplam tutar: " + total);
+
+	}
+
+}
