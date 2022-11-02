2fcd66e3f4a2e8150d67e05f20b9cbf23069029b
aysegul-sofuoglu <73445450+aysegul-sofuoglu@users.noreply.github.com>
2022-11-02 20:51:43 +0300

    exponential calculation

---
+++ java101/UsluSayiHesaplama.java
@@ -0,0 +1,26 @@
+package java101;
+import java.util.Scanner;
+
+public class UsluSayiHesaplama {
+
+	public static void main(String[] args) {
+		
+		int n, k;
+		int total = 1;
+		
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.println("Üssü alnınacak sayı: ");
+		n = input.nextInt();
+		System.out.println("Üs olacak sayı: ");
+		k = input.nextInt();
+		
+		for(int i =1; i<=k; i++) {
+			
+			total*=n;
+			
+		}
+		System.out.println("Cevap: " + total);
+	}
+
+}
