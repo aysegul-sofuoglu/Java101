d5829cd049e275a8399bd589c614218008529aee
aysegul-sofuoglu <73445450+aysegul-sofuoglu@users.noreply.github.com>
2022-11-03 13:51:10 +0300

    inverted triangle with stars

---
+++ java101/TersUcgen.java
@@ -0,0 +1,33 @@
+package java101;
+import java.util.Scanner;
+
+public class TersUcgen {
+
+	public static void main(String[] args) {
+		
+		Scanner inp = new Scanner(System.in);
+        System.out.print("Lütfen bir sayı giriniz");
+        int size = inp.nextInt();
+        
+        
+        
+        for (int i = size; i > 0; i--) { 
+        	
+            for (int k = size - i; k > 0; k--) { 
+                
+                System.out.print(" ");
+            }
+            
+            for (int j = (2*i)-1; j > 0; j--) { 
+                
+                System.out.print("*");
+            }
+            System.out.println();
+         
+
+	
+
+        }
+	}
+
+}
