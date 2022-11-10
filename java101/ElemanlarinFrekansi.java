17d41db8743a29b4845847f15a6e8c9217c10319
aysegul-sofuoglu <73445450+aysegul-sofuoglu@users.noreply.github.com>
2022-11-10 19:03:06 +0300

    number of repetitions of elements in the array

---
+++ java101/ElemanlarinFrekansi.java
@@ -0,0 +1,47 @@
+package java101;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ElemanlarinFrekansi {
+
+	public static void main(String[] args) {
+	
+		int array[] = {10, 20, 20, 10, 10, 20, 5, 20};
+		int count = 0;
+		
+		System.out.println("Dizi: " + Arrays.toString(array));
+		System.out.println("Tekrar Sayıları");
+		
+		
+		//sort array from smallest to largest
+		Arrays.sort(array);
+		
+
+		for(int i = 0; i < array.length; i++) {
+		     count=0;
+			for(int j = 0; j < array.length; j++) {
+				if(array[i] == array[j]) {
+					count++;
+				}
+			}
+			
+			
+			//look at the previous number and the next number
+			if(i == 0 && array[i + 1] == array[i]) {
+				System.out.println(String.format("%d sayısı %d kere tekrar edildi.", array[i], count));
+			}
+			
+			if(i == 0 && array[i + 1] != array[i]) {
+				System.out.println(String.format("%d sayısı %d kere tekrar edildi.", array[i], count));
+			}
+			
+			if(i != 0 && array[i - 1] != array[i]) {
+				System.out.println(String.format("%d sayısı %d kere tekrar edildi.", array[i], count));
+			}
+			
+			
+		}
+
+	}
+
+}
